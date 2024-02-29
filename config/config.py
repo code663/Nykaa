28ac1768a5c8917619c70a341c6d60818b9860df
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-02-29 23:49:26 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,10 +59,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL",
-    "https://t.me/ANGEL_K_WORLD",
+    "https://t.me/mr_editx",
 )  
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/IND_PAWAN",
+    "SUPPORT_GROUP", "https://t.me/angel_world11",
 )  # Example:- https://t.me/strangersuppor
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
